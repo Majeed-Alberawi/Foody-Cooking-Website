d31d30b10a9637b65b88ae9e57a65d4874984eff
Majeed Alberawi <majeedmousa0@gmail.com>
2022-05-11 23:18:52 +0300

    Uploaded Files

---
+++ layout/classic/js/main.js
@@ -0,0 +1,35 @@
+const checkBox = document.getElementById("themeBtn");
+const boll = document.querySelector(".theme-btn .boll");
+
+checkBox.addEventListener("click", () => {
+  boll.classList.toggle("checked");
+  document.body.classList.toggle("dark");
+});
+
+// include type //
+const cardTypes = document.querySelectorAll(".cards .card .nums .type .image");
+const cardTypesArr = Array.from(cardTypes);
+const cardTypeImage = document.querySelector(".cards .card .nums .type .image");
+// const cardTypeImageData = cardTypeImages.getAttribute("data-type");
+
+// console.log(cardTypeImages.getAttribute("data-type"));
+// console.log(cardTypesArr);
+
+/*
+cardTypes.forEach((cardType) => {
+  console.log(cardType.getAttribute("data-type"));
+  if ((cardType.dataset.type = "chiken")) {
+    cardTypeImage.innerHTML = `
+    <img src="./icons/type-icon/chicken.png" alt="" />
+    `;
+  } else if ((cardType.dataset.type = "meat")) {
+    cardTypeImage.innerHTML = `
+    <img src="./icons/type-icon/meat.png" alt="" />
+    `;
+  } else if ((cardType.dataset.type = "veg")) {
+    cardTypeImage.innerHTML = `
+    <img src="./icons/type-icon/salad.png" alt="" />
+    `;
+  }
+});
+*/
